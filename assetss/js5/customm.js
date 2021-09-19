f82ba3ac93c1778eba74d4ef8299194596d78c01
Laudari Sudip <53330430+Laudarisd@users.noreply.github.com>
2021-09-20 01:23:29 +0900

    Add files via upload

---
+++ assetss/js5/customm.js
@@ -0,0 +1,33 @@
+(function($) {
+    'use strict';
+    $(window).load(function() {
+    /* PreLoader */
+        $('.status').fadeOut();
+        $('.preloader').delay(350).fadeOut('slow');
+        /* Init GMaps */
+        $(".gmap").each(function() {
+            var a = $(this).attr('data-center');
+            $(this).mobileGmap({
+                markers: [{
+                    position: "center",
+                    info: a,
+                    showInfo: !0
+                }]
+            })
+        });
+    });
+    /* Backstretch */
+    /*$.backstretch('assets/images/background/home.png');*/
+    /* Animation Backstretch On Page Load */
+    
+    /* Init Text Rotator */
+    $(".description .sub").typed({
+        strings: ["AI Engineer ","Programmer "],
+        typeSpeed: 1,
+        backSpeed: 1,
+        backDelay: 1400,
+        loop: true
+    });
+    /* Optional Init Animation Reveal    new WOW().init(); */
+
+})(jQuery);
